26325213bcacc22fa55a9e6cdd9bad6545c40aba
li.min <li.min@adways.cn>
2016-12-15 11:02:19 +0800

    bubble sort n2

---
+++ BubbleSort.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var testArray = [61,12,78,24,33,69,30,47,56]
+func bubble_sort(array:[Int])->[Int]{
+    var myArray = array
+    let n = myArray.count
+    for _ in 0..<n-1 {
+        let j:Int = 0
+        for j in 0..<n-j-1 {
+            if myArray[j] > myArray[j+1] {
+                (myArray[j],myArray[j+1]) = (myArray[j+1],myArray[j])
+            }
+        }
+    }
+    return myArray;
+}
+
+var date1 = NSDate.init();
+bubble_sort(array: testArray)
+print("冒泡排序：",-date1.timeIntervalSinceNow)
